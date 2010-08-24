f869ae1bec4260b47faf93aa91f2aa68fcb2c4d4
jldupont <github@jldupont.com>
2010-08-24 10:47:46 -0400

    minor

--- src/musync/system/base.py
+++ src/musync/system/base.py
@@ -273,107 +273,6 @@ class AgentThreadedWithEvents(AgentThreadedBase):
                 
 
 
-class AgentThreadedBridge(object):
-    """
-    Base class for agents bridging the "gobject world" (example)
-    with the message based "mswitch world"
-    
-    *** NOT TESTED***
-    """
-    
-    LOW_PRIORITY_MESSAGE_BURST_SIZE=5
-    
-    def __init__(self, time_base):
-        """
-        @param time_base: in milliseconds
-        """
-        self.time_base=time_base 
-        self.ticks_second=1000/time_base
-
-        self.iq=Queue()
-        self.isq=Queue()
-        mswitch.subscribe(self.iq, self.isq)
-
-        self.tick_count=0
-        self.sec_count=0
-        self.min_count=0
-        self.hour_count=0
-        self.day_count=0
-        
-    def on_destroy(self):
-        """
-        Can be subclassed if any processing needs to
-        occur before terminating the thread
-        """
-        pass
-        
-    def tick(self, *_):
-        """
-        Performs message dispatch
-        """
-        tick_min=False
-        tick_hour=False
-        tick_day=False
-        tick_second = (self.tick_count % self.ticks_second) == 0 
-        self.tick_count += 1
-        
-        if tick_second:
-            self.sec_count += 1
-
-            tick_min=(self.sec_count % 60)==0
-            if tick_min:
-                self.min_count += 1
-                
-                tick_hour=(self.min_count % 60)==0
-                if tick_hour:
-                    self.hour_count += 1
-                    
-                    tick_day=(self.hour_count % 24)==0
-                    if tick_day:
-                        self.day_count += 1
-        
-        #print "tick! ", tick_second
-        mswitch.publish("__main__", "__tick__", self.ticks_second, 
-                        tick_second, tick_min, tick_hour, tick_day, 
-                        self.sec_count, self.min_count, self.hour_count, self.day_count)
-        
-        while True:
-            try:     
-                envelope=self.isq.get(False)
-                quit, mtype, handled=mdispatch(self, "__main__", envelope)
-                if handled==False:
-                    mswitch.publish(self.__class__, "__interest__", mtype, False, False, self.isq)
-                if quit:
-                    self.on_destroy()
-                    break
-                
-            except Empty:
-                break
-            continue            
-        
-        burst=self.LOW_PRIORITY_MESSAGE_BURST_SIZE
-        
-        while True:
-            try:     
-                envelope=self.iq.get(False)
-                quit, mtype, handled=mdispatch(self, "__main__", envelope)
-                if handled==False:
-                    mswitch.publish(self.__class__, "__interest__", mtype, False, False, self.iq)
-                if quit:
-                    self.on_destroy()
-                    break
-                    
-                burst -= 1
-                if burst == 0:
-                    break
-            except Empty:
-                break
-            
-            continue
-
-        ## Integration with gobject requires this
-        return True
-
 
 class TickGenerator(object):
     def __init__(self, ticks_second, publisher):
@@ -442,7 +341,7 @@ def custom_dispatch(source, q, pq, dispatcher, low_priority_burst_size=5):
             handled, snooping=dispatcher(mtype, *pargs)
             if handled==False:
                 print "* '%s': not interest in '%s' message type" % (orig, mtype)
-                mswitch.publish(source, "__interest__", source, mtype, False, snooping, pq)
+                mswitch.publish(source, "__interest__", source, mtype, False, snooping, q, pq)
                 break
         except Empty:
             break
@@ -462,7 +361,7 @@ def custom_dispatch(source, q, pq, dispatcher, low_priority_burst_size=5):
             handled, snooping=dispatcher(mtype, *pargs)
             if handled==False:
                 print "* '%s': not interest in '%s' message type" % (orig, mtype)
-                mswitch.publish(source, "__interest__", source, mtype, False, snooping, q)
+                mswitch.publish(source, "__interest__", source, mtype, False, snooping, q, pq)
             burst -= 1
             if burst == 0:
                 break
